2293d25960b31a927fbbd6a5b174747266347bec
Artem Belyshev <artembelyshev@MacBook-Pro.local>
2023-03-27 20:10:20 +0200

    inir nest

---
+++ src/items/items.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, Post, Put, Delete, Body, Req, Res, Param } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item-dto';
+import { Request, Response } from 'express';
+
+
+@Controller('items')
+export class ItemsController {
+  @Get()
+  // findAll(@Req() req: Request, @Res() res: Response): Response {
+  //   console.log(req.url);
+  //   return res.send("Check");
+  // }
+  findAll(): string {
+    return 'Got all items'
+  }
+
+  @Get(':id')
+  findOne(@Param() param): string {
+    return `Item: ${param.id}`;
+  }
+
+  @Post()
+  create(@Body() createItemDto: CreateItemDto): string {
+    return `Name: ${createItemDto.name}, Description: ${createItemDto.description}`;
+  }
+}
